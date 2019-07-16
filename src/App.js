5544c68b81fcee56ecd5c6f5b760b9319c872383
Eddie Chu <contact@eddiechu.me>
2019-07-15 19:42:12 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './sass/main.scss';
+
+export default class App extends React.Component {
+
+  render() {
+    return (
+        <div className="App">
+        </div>
+    );
+  }
+}
+
