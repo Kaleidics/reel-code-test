4581704753564a5118e1b1aa461777c3af4172cf
Eddie <42883353+Kaleidics@users.noreply.github.com>
2019-07-19 20:22:50 -0700

    set dynamic percent to progress

--- src/components/ProgressBar.js
+++ src/components/ProgressBar.js
@@ -4,7 +4,7 @@ export default class ProgressBar extends React.Component {
     render() {
         return (
             <div className="progress">
-                <div className="progress__bar shadow u-overlay-1" ><span className="progress__percent">{this.props.percent}%</span></div>
+                <div className="progress__bar shadow u-overlay-1" style={{width: `${this.props.percent}` }} ><span className="progress__percent">{this.props.percent}%</span></div>
             </div>
         );
     }
