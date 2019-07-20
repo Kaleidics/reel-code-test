cc7347bc67fa10a32959fc2019bdaa6d626a0509
Eddie <42883353+Kaleidics@users.noreply.github.com>
2019-07-19 20:14:48 -0700

    fix price typo

--- src/components/Reel.js
+++ src/components/Reel.js
@@ -14,7 +14,7 @@ export default class Reel extends React.Component {
                     <li className="reel__item">
                         <span className="reel__name u-margin-bottom-smallest">
                             <span className="product"><span className="u-break-long-words">{reel.name}</span><span className="u-font-color-grey u-font-size-small">{reel.status}</span></span>
-                            <span><span>$100</span>/<span>{reel.total}</span></span>
+                            <span><span>$100</span>/<span>${reel.total}</span></span>
                         </span>
                     </li>
                     <li className="reel__item">
